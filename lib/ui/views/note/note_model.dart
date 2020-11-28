f74c3341c3495716e04547db23cc37ea1a7909b8
dadop44 <58806599+dadop44@users.noreply.github.com>
2020-11-28 08:47:41 +0700

    update data model

--- lib/ui/views/note/note_model.dart
+++ lib/ui/views/note/note_model.dart
@@ -1,6 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
 class Note {
+  final String id =
+      UniqueKey().hashCode.toUnsigned(20).toRadixString(16).padLeft(5, '0');
   final String title;
   final String desc;
+  bool isDeleted = false;
 
   Note(this.title, this.desc);
+
+  static String get tableName => 'Note';
+
+  static String get createTable =>
+      'Create table $tableName(`id` TEXT PRIMARY KEY,'
+      ' `title` TEXT,'
+      ' `desc` TEXT,'
+      ' `isDeleted` INTEGER DEFAULT 0)';
 }
